f44cf5965d6d6539c2c5b1262db9d27ecfccf53d
rajpanchal <hemantkumar.2017@vitstudent.ac.in>
2018-06-24 11:48:21 +0530

    modified

--- js/events.js
+++ js/events.js
@@ -1,53 +1,65 @@
-
 $(document).ready(function(){
-  $('.eventcarousel').slick({
-  infinite: true,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  dots:true,
-  arrows:false,
-  centerMode:true,
-  prevArrow: $('.prev'),
-  nextArrow: $('.next'),
-  responsive: [
-    {
-      breakpoint: 900,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        centerMode:true
-      }
-    },
-    {
-      breakpoint: 680,
-      settings: {
+  $(".carousel").slick({
+    dots:false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay:true,
+    autoplayspeed:1000,
+    responsive:[
+      {breakpoint:1070,
+      settings:{
+        arrows:true,
         slidesToShow: 2,
-        slidesToScroll: 1,
-        centerMode:true
-      }
-    }
-  ],
-  autoplay:true,
-  autoplaySpeed:1000
-  });
+        slidesToScroll: 1
+      }},
+      {breakpoint:550,
+      settings:{
+        arrows:true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }},
+      {breakpoint:400,
+      settings:{
+        arrows:false,
+        dots:true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }}
 
-
-  $('#event1').click(function(){
-    //SHOW MODAL;
+    ]
   });
-  $('#event2').click(function(){
-    //SHOW MODAL;
-  });
-  $('#event3').click(function(){
-    //SHOW MODAL;
-  });
-  $('#event4').click(function(){
-    //SHOW MODAL;
-  });
-  $('#event5').click(function(){
-    //SHOW MODAL;
-  });
-  $('#event6').click(function(){
-    //SHOW MODAL;
+  $('.closebtn').on('click',function(){
+    $('.noeventinfo').fadeOut();
+    $('.eventinfo').fadeOut();
   });
 });
+
+var info=[
+  {
+    title:"GOV. SCHOOL VISIT",
+    img:["img2/ngo2.jpg","img2/ngo3 [1].jpg","img2/ngo2[2].jpg"],
+    information:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  }
+]
+function show(num){
+  $('.noeventinfo').fadeOut();
+  $('.eventinfo').fadeOut();
+  if(num<=info.length){
+    $('#eventinfoheader').text(info[num-1].title);
+    $('#eventinfobody').text(info[num-1].information);
+    if(screen.width<500){
+    $('.col').css('background-image','url('+info[num-1].img[0]+')');
+    $('.col').css('background-size','100% 100%');
+    }else{
+    $('#eventimg1').attr("src",info[num-1].img[0]);
+    $('#eventimg2').attr("src",info[num-1].img[1]);
+    $('#eventimg3').attr("src",info[num-1].img[2]);
+    }
+    console.log(1);
+    $('.eventinfo').fadeIn();
+
+  } else{
+    $('.noeventinfo').fadeIn();
+  }
+}
