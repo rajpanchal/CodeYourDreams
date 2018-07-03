43cd3fc222f97c70bba81d8a495832e8d200f5b7
vatsal-kandoi <vatsalkandoi1998@gmail.com>
2018-07-03 23:27:41 +0530

    Fixed sidebar close

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
     console.log(1);
+    $('.sidenav').sidenav();
+
     $('.colorblue').click(function(e){
         e.preventDefault();
+        
         var target=$(this).attr('href');
         $('html , body').animate({
             scrollTop:($(target).offset().top)
-        },500);
-    })
+        },700);
+        $('.sidenav').sidenav('close');
+    });
     $('#joinus').click(function(){
         $('html , body').animate({
             scrollTop:($('#joinus_link').offset().top)
-        },500);
-    })
+        },700);
+    });
+
 });
